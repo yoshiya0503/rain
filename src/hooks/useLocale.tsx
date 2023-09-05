b256ec0a217f11c03c65a4454caffd9709e92215
Ruki <myon53@gmail.com>
2023-09-05 19:02:46 +0900

    fix time locale

--- src/hooks/useLocale.tsx
+++ src/hooks/useLocale.tsx
@@ -1,10 +1,14 @@
+import _ from "lodash";
 import { useCallback } from "react";
 import { formatDistanceToNowStrict } from "date-fns";
 import { ja } from "date-fns/locale";
 
 export const useLocale = () => {
   const locale = useCallback((indexedAt: string) => {
-    return formatDistanceToNowStrict(Date.parse(indexedAt), { locale: ja });
+    if (_.includes(navigator.languages, "ja")) {
+      return formatDistanceToNowStrict(Date.parse(indexedAt), { locale: ja });
+    }
+    return formatDistanceToNowStrict(Date.parse(indexedAt));
   }, []);
 
   return { locale };
