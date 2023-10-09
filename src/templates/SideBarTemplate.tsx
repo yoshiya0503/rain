40df1ff3c1698e551f3c62b14f212ebe6d4bbdd8
Ruki <myon53@gmail.com>
2023-10-09 15:31:08 +0900

    fix template css

--- src/templates/SideBarTemplate.tsx
+++ src/templates/SideBarTemplate.tsx
@@ -9,12 +9,12 @@ export const MenuTemplate = () => {
       <Skeleton variant="rounded" height={40} />
       <Paper component="nav" sx={{ width: 360, height: 300, p: 2, borderRadius: 3 }}>
         <Stack spacing={3}>
-          {_.times(4, (key) => (
-            <Stack key={key} direction="row" alignItems="center" spacing={2}>
-              <Skeleton width={48} height={48} variant="rounded" />
-              <Stack direction="column" spacing={1} sx={{ flexGrow: 1 }}>
-                <Skeleton height={20} />
-                <Skeleton height={20} />
+          {_.times(5, (key) => (
+            <Stack key={key} direction="row" alignItems="center" spacing={1}>
+              <Skeleton width={32} height={32} variant="rounded" />
+              <Stack direction="column" sx={{ flexGrow: 1 }}>
+                <Skeleton height={15} />
+                <Skeleton height={15} />
               </Stack>
             </Stack>
           ))}
@@ -22,12 +22,12 @@ export const MenuTemplate = () => {
       </Paper>
       <Paper component="nav" sx={{ width: 360, height: 300, p: 2, borderRadius: 3 }}>
         <Stack spacing={3}>
-          {_.times(3, (key) => (
+          {_.times(4, (key) => (
             <Stack key={key} direction="row" alignItems="center" spacing={2}>
-              <Skeleton width={64} height={64} variant="circular" />
-              <Stack direction="column" spacing={1} sx={{ flexGrow: 1 }}>
-                <Skeleton height={20} />
-                <Skeleton height={20} />
+              <Skeleton width={48} height={48} variant="circular" />
+              <Stack direction="column" sx={{ flexGrow: 1 }}>
+                <Skeleton height={15} />
+                <Skeleton height={15} />
               </Stack>
             </Stack>
           ))}
